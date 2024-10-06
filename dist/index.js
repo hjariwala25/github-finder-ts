119bc865736aec37843310ecf785a931cc4c41cf
hjariwala25 <hjariwala150@gmail.com>
2024-10-06 19:08:17 +0530

    update container

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,33 @@
 "use strict";
 const getUsername = document.querySelector("#user");
 const formSubmit = document.querySelector(".form");
-const main_container = document.querySelector(".main-container");
+const main_container = document.querySelector(".main_container");
 async function myCustomFetcher(url, options) {
     const response = await fetch(url, options);
     if (!response.ok) {
         throw new Error(`Network response was not ok - status: ${response.status}`);
     }
-    const data = response.json();
+    const data = await response.json();
     console.log(data);
     return data;
 }
+const showResult = (singleUser) => {
+    const { avatar_url, login, url, location } = singleUser;
+    main_container.insertAdjacentHTML("beforeend", `<div class='card'>
+        <img src=${avatar_url} alt=${login}/>
+        <hr/>
+        <div class="card-footer">
+        <img src="${avatar_url}" alt="${login}/>
+        <a href="${url}">Github </a>
+        </div>
+        </div>
+        `);
+};
 function fetchUserData(url) {
-    myCustomFetcher(url, {});
+    myCustomFetcher(url, {}).then((userInfo) => {
+        for (const singleUser of userInfo) {
+            showResult(singleUser);
+        }
+    });
 }
 fetchUserData("https://api.github.com/users");
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const getUsername = document.querySelector("#user") as HTMLInputElement;
 const formSubmit = document.querySelector(".form") as HTMLFormElement;
-const main_container = document.querySelector(".main-container") as HTMLElement;
+const main_container = document.querySelector(".main_container") as HTMLElement;
 
 
 interface UserData{
@@ -18,15 +18,35 @@ async function myCustomFetcher<T>(url:string, options?:RequestInit):Promise<T>{
             `Network response was not ok - status: ${response.status}`
         );
     }
-    const data = response.json();
+    const data = await response.json();
     console.log(data);
     
     return data;
  
 }
 
+const showResult = (singleUser: UserData) => {
+    const {avatar_url, login, url, location} = singleUser;
+    main_container.insertAdjacentHTML(
+        "beforeend",
+        `<div class='card'>
+        <img src=${avatar_url} alt=${login}/>
+        <hr/>
+        <div class="card-footer">
+        <img src="${avatar_url}" alt="${login}/>
+        <a href="${url}">Github </a>
+        </div>
+        </div>
+        `
+    )
+}
+
 function fetchUserData(url: string){
-    myCustomFetcher<UserData[]>(url, {});
+    myCustomFetcher<UserData[]>(url, {}).then((userInfo) => {
+        for(const singleUser of userInfo){
+            showResult(singleUser);
+        }
+    })
 }
 
 fetchUserData("https://api.github.com/users");
